29b96f75594f8bac06fd10ec768997f21816cef8
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-12-02 21:38:52 +0300

    deploy

--- partials/header.php
+++ partials/header.php
@@ -9,7 +9,8 @@
 
     <div class="header__logo">
       <a href="/">
-        <img src="<?php bloginfo('template_url') ?>/dist/images/logo.png" alt="<?php bloginfo('name') ?>" />
+        vvedenie-but.ru
+        <!-- <img src="<?php bloginfo('template_url') ?>/dist/images/logo.png" alt="<?php bloginfo('name') ?>" /> -->
       </a>
     </div>
 
@@ -18,26 +19,5 @@
       'theme_location' => 'menu-main',
       'menu_class' => 'header__menu'
     ]); ?>
-
-    <div class="header-contacts">
-      <div class="header-contacts__links">
-        <a href="tel:<?php the_field('theme_phone', 'options') ?>" class="header-contacts__phone js-callback-or-modal">
-          <?php the_field('theme_phone', 'options') ?>
-        </a>
-        <a href="mailto:<?php the_field('theme_email', 'options') ?>" class="header-contacts__email">
-          <svg xmlns="http://www.w3.org/2000/svg" width="19px" height="14px" viewBox="0 0 15 11">
-            <path d="M0.794,10.758 L14.325,10.758 C14.362,10.758 14.398,10.750 14.430,10.735 L9.794,6.388 L7.559,9.093 L5.325,6.388 L0.689,10.735 C0.721,10.750 0.757,10.758 0.794,10.758 ZM0.547,10.383 L4.580,5.486 L0.547,0.604 L0.547,10.383 ZM0.869,0.240 L0.985,0.328 L7.559,7.340 L14.134,0.328 L14.250,0.240 L0.869,0.240 ZM14.572,0.604 L10.539,5.486 L14.572,10.383 L14.572,0.604 Z" />
-          </svg>
-          <?php the_field('theme_email', 'options') ?>
-        </a>
-      </div>
-
-      <button class="header-contacts__button" data-hystmodal="#feedback">
-        <svg xmlns="http://www.w3.org/2000/svg" width="19px" height="14px" viewBox="0 0 15 11">
-          <path d="M0.794,10.758 L14.325,10.758 C14.362,10.758 14.398,10.750 14.430,10.735 L9.794,6.388 L7.559,9.093 L5.325,6.388 L0.689,10.735 C0.721,10.750 0.757,10.758 0.794,10.758 ZM0.547,10.383 L4.580,5.486 L0.547,0.604 L0.547,10.383 ZM0.869,0.240 L0.985,0.328 L7.559,7.340 L14.134,0.328 L14.250,0.240 L0.869,0.240 ZM14.572,0.604 L10.539,5.486 L14.572,10.383 L14.572,0.604 Z" />
-        </svg>
-      </button>
-    </div>
-
   </div>
 </section>
--- template-home.php
+++ template-home.php
@@ -2,33 +2,12 @@
 /*
 Template Name: Главная
 */
-$sticky = get_option('sticky_posts');
-$sticky_posts = new WP_Query([
-	'posts_per_page' => 3,
-  'post_type' => 'post',
-  'order' => 'DESC',
-  'orderby' => 'date',
-  'cat' => 7,
-  'post__in' => $sticky,
-]);
 $post_news = new WP_Query([
 	'posts_per_page' => 3,
   'post_type' => 'post',
   'order' => 'DESC',
   'orderby' => 'date',
   'cat' => 5,
-]);
-$products = new WP_Query([
-  'post_type' => 'page',
-  'post_parent' => 52,
-  'order' => 'ASC',
-  'orderby' => 'menu_order',
-]);
-$service_pages = new WP_Query([
-  'post_type' => 'page',
-  'post_parent' => 11,
-  'order' => 'ASC',
-  'orderby' => 'menu_order',
 ]);
 ?>
 <!DOCTYPE html>
@@ -70,70 +49,6 @@ $service_pages = new WP_Query([
       </section>
       <?php endif; ?>
 
-      <?php if ($services = get_field('services')): ?>
-      <section class="home-services">
-        <div class="ui-container">
-          <div class="home-services__title"><?php echo $services['title'] ?></div>
-          <div class="home-services__list">
-            <?php while ($service_pages->have_posts()): ?>
-            <?php $service_pages->the_post()?>
-            <div class="home-services__item">
-              <article class="home-services-card">
-                <?php if ($image = get_field('list_thumbnail', get_the_ID())): ?>
-                <figure class="home-services-card__image">
-                  <img src="<?php echo $image['url'] ?>" alt="<?php the_title()?>" />
-                </figure>
-                <?php endif; ?>
-                <h2 class="home-services-card__title">
-                  <a href="<?php the_permalink() ?>" class="home-services-card__link"><?php the_title() ?></a>
-                </h2>
-              </article>
-            </div>
-            <?php endwhile?>
-            <?php wp_reset_postdata()?>
-          </div>
-          <?php if ($services['content_title'] || $services['content_text']): ?>
-          <div class="home-services-content">
-            <?php if ($services['content_title']): ?>
-            <div class="home-services-content__title">
-              <?php echo $services['content_title'] ?>
-            </div>
-            <?php endif; ?>
-            <?php if ($services['content_text']): ?>
-            <div class="home-services-content__text">
-              <?php echo $services['content_text'] ?>
-            </div>
-            <?php endif; ?>
-          </div>
-          <?php endif; ?>
-        </div>
-      </section>
-      <?php endif; ?>
-
-      <?php if ($advantages = get_field('advantages')): ?>
-      <section class="home-advantages">
-        <div class="ui-container">
-          <div class="home-advantages__title"><?php echo $advantages['title'] ?></div>
-          <div class="home-advantages__list">
-            <div class="home-advantages-list">
-              <?php foreach ($advantages['list'] as $key => $item): ?>
-              <div class="home-advantages-list__item">
-                <article class="home-advantages-card">
-                  <div class="home-advantages-card__number">
-                    <?php echo $key + 1 ?>
-                  </div>
-                  <div class="home-advantages-card__title">
-                    <?php echo $item['text'] ?>
-                  </div>
-                </article>
-              </div>
-              <?php endforeach; ?>
-            </div>
-          </div>
-        </div>
-      </section>
-      <?php endif; ?>
-
       <?php if ($post_news->have_posts()): ?>
       <section class="home-news">
         <div class="ui-container">
@@ -165,127 +80,6 @@ $service_pages = new WP_Query([
               </div>
               <?php endwhile?>
               <?php wp_reset_postdata()?>
-            </div>
-          </div>
-        </div>
-      </section>
-      <?php endif; ?>
-
-      <section class="home-projects">
-        <div class="ui-container">
-          <div class="home-projects-layout">
-            <div class="home-projects-layout__section">
-              <div class="home-projects-section">
-                <div class="home-projects-section__title">Наши объекты</div>
-                <div class="home-projects-section__date">с 2013 по 2020 годы</div>
-                <a href="#" class="home-projects-section__more">Смотреть все</a>
-              </div>
-            </div>
-            <div class="home-projects-layout__info">
-              <div class="home-projects-info">
-                <div class="home-projects-info__title">
-                  Многофункциональный Распределительный Центр X5. 36 000 м2
-                </div>
-                <div class="home-projects-info__desc">
-                  Воронежская обл. Рамонский район, Айдаровское сельское поселение, Территория промышленная зона 4. II кв. 2014 г.- II кв. 2015
-                </div>
-              </div>
-            </div>
-            <div class="home-projects-layout__gallery">
-              <div class="swiper projects-swiper-main">
-                <div class="swiper-wrapper">
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                </div>
-              </div>
-              <div thumbsSlider class="swiper projects-swiper-thumbs">
-                <div class="swiper-wrapper">
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                  <div class="swiper-slide">
-                    <img src="<?php bloginfo('template_url') ?>/dist/images/slide.png" />
-                  </div>
-                </div>
-                <div class="swiper-button-next"></div>
-                <div class="swiper-button-prev"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-
-      <?php if ($partners = get_field('partners')): ?>
-      <section class="home-partners">
-        <div class="ui-container">
-          <div class="home-partners__title"><?php echo $partners['title'] ?></div>
-          <div class="home-partners__list">
-            <div class="home-partners-list">
-              <?php foreach ($partners['items'] as $key => $item): ?>
-              <div class="home-partners-list__item">
-                <div class="home-partners-item">
-                  <img src="<?php echo $item['image']['url'] ?>" alt="" class="home-partners-item__image">
-                  <?php if ($item['description']): ?>
-                    <div class="home-partners-item__description">
-                      <?php echo $item['description'] ?>
-                    </div>
-                  <?php endif; ?>
-                </div>
-              </div>
-              <?php endforeach; ?>
             </div>
           </div>
         </div>
