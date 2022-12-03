bfef212ee292748297d2dbf5aa38e6ecf85a9ca5
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-12-03 13:46:12 +0300

    deploy

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <body <?php body_class();?>>
     <?php wp_body_open();?>
 
-    <div class="ui-wrapper">
+    <div class="page-wrapper">
       <?php get_template_part('partials/header') ?>
       <?php get_template_part('partials/page-breadcrumbs') ?>
       <?php get_template_part('partials/page-headline') ?>
