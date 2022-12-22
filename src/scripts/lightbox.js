67d9f79807075129dce5dc58565ad0067f1a8cc2
kundius <kundius.ruslan@gmail.com>
2022-12-22 04:45:01 +0300

    deploy

--- src/scripts/lightbox.js
+++ src/scripts/lightbox.js
@@ -4,4 +4,8 @@ const links = document.querySelectorAll('.wp-block-image a') || []
 links.forEach(link => {
   link.setAttribute("data-fslightbox", "gallery")
 })
+const links2 = document.querySelectorAll('.kb-gallery-figure a') || []
+links2.forEach(link => {
+  link.setAttribute("data-fslightbox", "gallery")
+})
 refreshFsLightbox();
